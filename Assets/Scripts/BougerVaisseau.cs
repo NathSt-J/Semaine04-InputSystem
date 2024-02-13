aadd814aa32c7310495d706b95951329fa46d263
NathSt-J <nathanstjean362@gmail.com>
2024-02-13 12:04:12 -0500

    Préparation script Player

---
+++ Assets/Scripts/BougerVaisseau.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BougerVaisseau : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Bouger(){
+
+    }
+
+    public void RecevoirInfo(InputAction.CallbackContext context){
+        Debug.Log(context.ReadValue<Vector2>());
+    }
+}
